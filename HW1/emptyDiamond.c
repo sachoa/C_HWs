d1977027f531c0876f57e72fbe6310d940bb171e
stephanieachoa <stephanie.achoa@gmail.com>
2021-09-07 10:59:12 -0400

    empty diamond complete with results file

--- HW1/emptyDiamond.c
+++ HW1/emptyDiamond.c
@@ -11,30 +11,38 @@
 
 int emptyDiamond () {
 
-    int size, rows, spaces, print, line;
+    int n, size, rows, spaces, print, line;
 
     printf("Enter diamond size (must be an odd integer) \n");
     scanf("%d", &size);
 
     rows = (size+1)/2; //top half of the rows counting the center
 
-    if (size > 0 && size %2 != 0){
-        for (line=1; line <= rows; line++){ //line is the current line I'm "standing" on
 
-            for (spaces = 1; spaces <= rows - line; spaces++)
-                printf(" ");
-
-            for (print = 1; print <= 2*line-1; print++)
-                printf("B");
-            printf("\n");
-        }
+   if (size > 0 && size %2 != 0){
+       for (line=1; line <= rows; line++){ //line is the current line I'm "standing" o
+           for (spaces = 1; spaces <= rows - line; spaces++)
+               printf(" ");
+           for (print = 1; print <= 2*line-1; print++){
+               if (print==1 || print==(2*line-1)){
+                   printf("B");
+               } else{
+                   printf(" ");
+               }
+           }
+           printf("\n");
+       }
 
         for (line= rows-1; line >= 1; line--){
             for (spaces = 1; spaces <= rows - line; spaces++)
                 printf(" ");
 
             for (print = 1; print <= 2*line-1; print++)
-                printf("B");
+                if (print==1 || print==(2*line-1)){
+                    printf("B");
+                } else{
+                    printf(" ");
+                }
             printf("\n");
         }
 
@@ -42,9 +50,6 @@ int emptyDiamond () {
     else {
         printf("Enter a positive and odd integer \n");
     }
-
-
-
 
     return 0;
 }
