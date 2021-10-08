c844fde081f07c48b09c8490514816592ec63df4
stephanieachoa <stephanie.achoa@gmail.com>
2021-10-08 15:50:16 -0400

    Josephus in progress

---
+++ HW5/josephus_functions.h
@@ -0,0 +1,11 @@
+//
+// Created by Stephanie Achoa on 10/5/21.
+//
+
+#ifndef C_HWS_JOSEPHUS_FUNCTIONS_H
+#define C_HWS_JOSEPHUS_FUNCTIONS_H
+
+static *create_rebels(int);
+int josephus(int, int);
+int num_rebels, increment;
+#endif //C_HWS_JOSEPHUS_FUNCTIONS_H
